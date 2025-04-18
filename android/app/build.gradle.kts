6f9ee0e232eed94c44761aea7b6c193cc557deda
mariamomer135 <140013469+mariamomer135@users.noreply.github.com>
2025-04-18 10:58:19 -0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") // ✅ Correct way to apply Firebase plugin
+    id("com.google.gms.google-services") 
 }
 
 android {
